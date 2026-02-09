9892f42e9c2d0b3ce6f954daae297c17f1604626
BenjaminDThomas <benthoma0s03@gmail.com>
2026-02-09 23:01:53 +0000

    add dark/light mode toggle and account authentication process

--- student_magazine/js/main.js
+++ student_magazine/js/main.js
@@ -6,10 +6,12 @@ Scroll To Top Button
 
 const btn = document.querySelector("#scrollTopBtn");                    // selects 'scrollTopBtn' class
 
-window.addEventListener("scroll", () => {                               // checks for scrolling interaction
-  btn.style.display = window.scrollY > 500 ? "block" : "none";          // button only appears after 500+ y position
-});
+if (btn) {                                                              // only runs if slider exists on the page
+    window.addEventListener("scroll", () => {                           // checks for scrolling interaction
+    btn.style.display = window.scrollY > 500 ? "block" : "none";        // button only appears after 500+ y position
+    });
 
-btn.addEventListener("click", () => {                                   // on scroll to top button click event
-  window.scrollTo({ top: 0, behavior: "smooth" });                      // scrolls to top of page
-});
+    btn.addEventListener("click", () => {                               // on scroll to top button click event
+    window.scrollTo({ top: 0, behavior: "smooth" });                    // scrolls to top of page
+    })
+};
